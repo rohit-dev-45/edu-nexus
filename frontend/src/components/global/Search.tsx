cffb3e8ff1ee03acfc54046818650892b913d7df
rohit-dev-45 <rohit137sh@gmail.com>
2026-08-30 16:44:14 +0000

    06: Global Components

---
+++ frontend/src/components/global/Search.tsx
@@ -0,0 +1,24 @@
+import { Input } from "@/components/ui/input";
+import { SearchIcon } from "lucide-react";
+
+export const Search = ({
+    search,
+    setSearch,
+    title,
+}: {
+    search: string;
+    setSearch: (search: string) => void;
+    title: string;
+}) => {
+    return (
+        <div className="relative w-full md:w-64">
+            <SearchIcon className="absolute left-2 top-2.5 size-4 text-muted-foreground" />
+            <Input
+                placeholder={`Search ${title}`}
+                className="pl-8"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+            />
+        </div>
+    );
+};
