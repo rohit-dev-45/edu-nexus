2bec0c02579586fd901ebc8cfcc6eddf4c374725
rohit-dev-45 <rohit137sh@gmail.com>
2026-08-30 15:22:51 +0000

    04: Classes

---
+++ frontend/src/components/global/Modal.tsx
@@ -0,0 +1,41 @@
+import {
+    Dialog,
+    DialogContent,
+    DialogClose,
+    DialogFooter,
+    DialogHeader,
+    DialogDescription,
+    DialogTitle,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+
+export const Modal = ({
+    description,
+    open,
+    setOpen,
+    title,
+    children,
+}: {
+    title: string;
+    description: string;
+    setOpen: (open: boolean) => void;
+    open: boolean;
+    children: React.ReactNode;
+}) => {
+    return (
+        <Dialog open={open} onOpenChange={setOpen}>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>{title}</DialogTitle>
+                    <DialogDescription>{description}</DialogDescription>
+                </DialogHeader>
+                {children}
+                <DialogFooter>
+                    <DialogClose asChild>
+                        <Button variant="outline">Cancel</Button>
+                    </DialogClose>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
+    );
+};
