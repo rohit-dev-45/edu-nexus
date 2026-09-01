5398cf1a16e85d05d20ea8cfcd3d8096f50fa023
rohit-dev-45 <rohit137sh@gmail.com>
2026-09-01 14:23:38 +0000

    13: Users Components

---
+++ frontend/src/components/users/dialog.tsx
@@ -0,0 +1,39 @@
+import type { user, UserRole } from "@/types";
+import { Modal } from "../global/Modal";
+import { UniversalUserForm } from "../auth/form";
+
+export const UserDialog = ({
+    open,
+    setOpen,
+    editingUser,
+    role,
+    onSuccess,
+}: {
+    setOpen: (open: boolean) => void;
+    open: boolean;
+    editingUser: user | null;
+    role: UserRole;
+    onSuccess?: () => void;
+}) => {
+    const title = editingUser ? "Update User" : "Create User";
+    const description = editingUser ? "Update user details" : "Add a new user";
+    const onSuccessPlus = () => {
+        setOpen(false);
+        onSuccess?.();
+    };
+    return (
+        <Modal
+            title={title}
+            description={description}
+            open={open}
+            setOpen={setOpen}
+        >
+            <UniversalUserForm
+                type={editingUser ? "update" : "create"}
+                role={role}
+                initialData={editingUser}
+                onSuccess={onSuccessPlus}
+            />
+        </Modal>
+    );
+};
