55cacadf845b46b0bd7767348f6bae356d5b065a
rohit-dev-45 <rohit137sh@gmail.com>
2026-09-01 14:58:05 +0000

    21: Frontend Completed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,113 @@
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import { PrivateRoutes } from "./components/PrivateRoutes";
+import Dashboard from "./pages/Dashboard";
+import AcademicYear from "./pages/settings/academic-year";
+import UserManagementPage from "./pages/users";
+import Classes from "./pages/academics/Classes";
+import { Subjects } from "./pages/academics/Subjects";
+import Timetable from "./pages/academics/Timetable";
+import Exams from "./pages/lms/Exams";
+import Exam from "./pages/lms/Exam";
+
+const router = createBrowserRouter([
+    {
+        children: [
+            // Public routes
+            {
+                index: true,
+                element: <Home />,
+            },
+            {
+                path: "login",
+                element: <Login />,
+            },
+
+            // Protected routes
+            {
+                element: <PrivateRoutes />,
+                children: [
+                    {
+                        path: "dashboard",
+                        element: <Dashboard />,
+                    },
+                    {
+                        path: "activities-log",
+                        element: <Dashboard />,
+                    },
+                    {
+                        path: "settings/academic-years",
+                        element: <AcademicYear />,
+                    },
+                    {
+                        path: "users/students",
+                        element: (
+                            <UserManagementPage
+                                role="student"
+                                title="Students"
+                                description="Manage student directory and class assignments."
+                            />
+                        ),
+                    },
+                    {
+                        path: "users/teachers",
+                        element: (
+                            <UserManagementPage
+                                role="teacher"
+                                title="Teachers"
+                                description="Manage teaching staff."
+                            />
+                        ),
+                    },
+                    {
+                        path: "users/parents",
+                        element: (
+                            <UserManagementPage
+                                role="parent"
+                                title="Parents"
+                                description="Manage Parents."
+                            />
+                        ),
+                    },
+                    {
+                        path: "users/admins",
+                        element: (
+                            <UserManagementPage
+                                role="admin"
+                                title="Admins"
+                                description="Manage Admins."
+                            />
+                        ),
+                    },
+                    {
+                        path: "classes",
+                        element: <Classes />,
+                    },
+                    {
+                        path: "subjects",
+                        element: <Subjects />,
+                    },
+                    {
+                        path: "timetable",
+                        element: <Timetable />,
+                    },
+                    {
+                        path: "lms/exams",
+                        element: <Exams />,
+                    },
+                    {
+                        path: "lms/exams/:id",
+                        element: <Exam />,
+                    },
+                ],
+            },
+        ],
+    },
+]);
+
 const App = () => {
-    return <div>App</div>;
+    return <RouterProvider router={router} />;
 };
 
 export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Toaster } from "sonner";
+
 import "./index.css";
-import App from "./App.tsx";
+
+import App from "./App";
+import { AuthProvider } from "@/hooks/AuthProvider";
+import { ThemeProvider } from "@/components/provider/theme";
 
 createRoot(document.getElementById("root")!).render(
-    <StrictMode>
-        <App />
-    </StrictMode>,
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <StrictMode>
+            <AuthProvider>
+                <App />
+                <Toaster />
+            </AuthProvider>
+        </StrictMode>
+    </ThemeProvider>,
 );
